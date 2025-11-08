270d7b46859e74b9e0ef1353b3b7b782c7e033b8
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-08 20:20:56 +0700

    Update env.sh

--- device/xiaomi-fog/env.sh
+++ device/xiaomi-fog/env.sh
@@ -11,18 +11,17 @@ PORT_TYPE=hybris
 HYBRIS_VER=18.1
 ANDROID_MAJOR_VERSION=11
 #REPO_INIT_URL="https://github.com/SailfishOS-miatoll/android.git"
-REPO_INIT_URL="https://github.com/mer-hybris/android.git"
+REPO_INIT_URL="https://github.com/SailfishOS-msmnile/manifest.git"
 REPO_LOCAL_MANIFESTS_URL="https://github.com/SourceLab081/local_manifests"
 #HAL_MAKE_TARGETS=(hybris-hal droidmedia bzip2 libbiometry_fp_api)
-#HAL_MAKE_TARGETS=(hybris-hal droidmedia bzip2 libbiometry_fp_api)
-HAL_MAKE_TARGETS=(libui_compat_layer hybris-hal droidmedia libsfplugin_ccodec)
+HAL_MAKE_TARGETS=(hybris-hal droidmedia)
 HAL_ENV_EXTRA=""
 RELEASE=5.0.0.67
 TOOLING_RELEASE=5.0.0.62
 SDK_RELEASE=latest
-REPOS=(
-    'https://gitlab.com/TheXPerienceProject/yuki_clang.git' prebuilts/yuki-clang "18.0.0" 1
-    'https://github.com/mer-hybris/libhybris.git' hybris/mw/libhybris "master" 1
-)
+#REPOS=(
+ #   'https://gitlab.com/TheXPerienceProject/yuki_clang.git' prebuilts/yuki-clang "18.0.0" 1
+ #   'https://github.com/mer-hybris/libhybris.git' hybris/mw/libhybris "master" 1
+#)
 #LINKS=()
 export VENDOR DEVICE PORT_ARCH RELEASE

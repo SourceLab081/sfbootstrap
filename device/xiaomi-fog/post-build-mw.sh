1e331504017c0085d5839d0cd0b4da994aaa08d3
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-24 10:12:19 +0700

    Add new MW build packages for SailfishOS
    Added new build packages for SailfishOS.

--- device/xiaomi-fog/post-build-mw.sh
+++ device/xiaomi-fog/post-build-mw.sh
@@ -1,3 +1,5 @@
+sfb_build_packages --mw=https://github.com/SailfishOS-vayu/parse-android-dynparts.git && \
+sfb_build_packages --mw=https://github.com/mentaljam/harbour-storeman.git
 #sfb_build_packages --build=hybris/droid-system-miatoll/ && \
 #sfb_build_packages --build=hybris/mw/sailfish-fpd-community --spec=rpm/droid-biometry-fp.spec --do-not-install && \
 #sfb_build_packages --build=hybris/mw/sailfish-fpd-community --spec=rpm/sailfish-fpd-community.spec --do-not-install
